c4ca5ed118711e43179014981b36abb6c7b70fb6
Iris <78657554+RaiHormo@users.noreply.github.com>
2026-05-28 18:15:09 +0300

    fixed some issues

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 cp eduroam-universal.py Appdir/script.py
-ARCH=x86_64 appimagetool.AppImage AppDir
-mv Eduroam_Linux_Universal_Installer-x86_64.AppImage 'Eduroam Linux Installer.AppImage'
+ARCH=x86_64 ~/Applications/appimagetool.AppImage AppDir
+mv Eduroam_Linux_Universal_Installer-x86_64.AppImage 'Eduroam-Linux-Installer.AppImage'
